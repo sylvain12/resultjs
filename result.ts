ad1e9dd097ab25fbe13d6a69e4d3e920afb3340f
Sylvain Kadjo <sylvainka12@gmail.com>
2023-10-04 21:38:48 +0400

    Implement UnwrapError, unwrap and unwrapErr
    - Add custom Error (UnwrapError) to capture not allowed Result from either Err or Ok
    - Imlplement unwrapErr for Ok Result
    - Implement unwrap for Err Result

--- result.ts
+++ result.ts
@@ -1,4 +1,4 @@
-
+//------------------- Ok Class --------------------
 export class Ok<T> {
 	private _value: T;
 	constructor(value: T) {
@@ -25,13 +25,22 @@ export class Ok<T> {
 		return null;
 	}
 
-	// TODO
 	unwrap(): T {
-		return this._value
+		return this._value;
 	}
-	unwrapErr() {}
+
+	/**
+	 * Throws an UnwrapError with a descriptive message when attempting to use unwrapErr on an 'Ok' value.
+	 * @function
+	 * @throws {UnwrapError} - An error indicating that 'unwrapErr()' was called on an 'Ok' value.
+	 * @returns {void}
+	 */
+	unwrapErr() {
+		throw new UnwrapError(`Called 'Result.unwrapErr()' on an 'Ok' value: ${this._value?.toString()}`);
+	}
 }
 
+//------------------- Err Class --------------------
 export class Err<E> {
 	private _value: E;
 
@@ -58,13 +67,15 @@ export class Err<E> {
 		return null;
 	}
 
-	// TODO
-	unwrap() {}
+	unwrap(): void {
+		throw new UnwrapError(`Called 'Result.unwrap()' on an 'Err' value: ${this._value?.toString()}`);
+	}
 	unwrapErr(): E {
 		return this._value;
 	}
 }
 
+//------------------- Result type --------------------
 export type Result<T, E> = Ok<T> | Err<E>;
 
 
@@ -73,4 +84,18 @@ export const matchresult = <T, E>(result: Result<T, E>) => {
 		ok: result.okValue,
 		err: result.errValue,
 	};
+}
+
+
+//------------------- Custom Errors --------------------
+/**
+ * Represents an error that occurs when attempting to unwrap a value.
+ * @class
+ * @extends Error
+ * @param {string} message - The error message.
+ */
+class UnwrapError extends Error {
+	constructor(message: string) {
+		super(message);
+	}
 }
