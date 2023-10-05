75a4c6a11405e76397968a1195afb2bbccd46abb
Sylvain Kadjo <sylvainka12@gmail.com>
2023-10-05 22:14:03 +0400

    Documentation
    - Add documentation for 'Err' class
    - Edit docs for 'Ok' class

--- result.ts
+++ result.ts
@@ -3,7 +3,7 @@
 /**
  * Represents the 'Ok' state of a Result object.
  * @class
- * @type {T} - 'Ok' value generic type
+ * @template {T} - Generic type for 'Ok'
  */
 export class Ok<T> {
 	private _value: T;
@@ -14,7 +14,7 @@ export class Ok<T> {
 	/**
 	 * Checks if the Result is in 'Ok' state.
 	 * @function
-	 * @returns {boolean} - Return true for 'Ok' value
+	 * @returns {boolean}
 	 */
 	isOK() {
 		return true;
@@ -23,16 +23,16 @@ export class Ok<T> {
 	/**
 	 * Checks if the Result is in 'Err' state.
 	 * @function
-	 * @returns {boolean} - Return false for 'Ok' value
+	 * @returns {boolean}
 	 */
 	isErr() {
 		return false;
 	}
 
 	/**
-	 * Retrieves the wrapped value in the 'Ok' state.
+	 * Retrieves value in the 'Ok' state.
 	 * @function
-	 * @returns {T} - 'Ok' value type
+	 * @returns {T}
 	 */
 	ok(): T {
 		return this._value;
@@ -41,17 +41,17 @@ export class Ok<T> {
 	/**
 	 * Retrieves the 'Err' value for Result in 'Ok' state.
 	 * @function
-	 * @returns {null} - The wrapped value is always null.
+	 * @returns {null}
 	 */
 	err(): null {
 		return null;
 	}
 
 	/**
-	 * Gets the wrapped value in the 'Ok' state.
+	 * Gets value in the 'Ok' state.
 	 * @public
 	 * @readonly
-	 * @returns {T} - 'Ok' value type
+	 * @returns {T}
 	 */
 	public get okValue(): T {
 		return this._value;
@@ -61,7 +61,7 @@ export class Ok<T> {
 	 * Gets 'Err' value for Result in 'Ok' state.
 	 * @public
 	 * @readonly
-	 * @returns {null} - The wrapped value is always null.
+	 * @returns {null}
 	 */
 	public get errValue(): null {
 		return null;
@@ -70,7 +70,7 @@ export class Ok<T> {
 	/**
 	 * Retrieves the wrapped value.
 	 * @function
-	 * @returns {T} - 'Ok' value type
+	 * @returns {T}
 	 */
 	unwrap(): T {
 		return this._value;
@@ -79,7 +79,7 @@ export class Ok<T> {
 	/**
 	 * Throws an UnwrapError with a descriptive message when attempting to use unwrapErr on an 'Ok' value.
 	 * @function
-	 * @throws {UnwrapError} - An error indicating that 'unwrapErr()' was called on an 'Ok' value.
+	 * @throws {UnwrapError}
 	 * @returns {void}
 	 */
 	unwrapErr() {
@@ -88,35 +88,90 @@ export class Ok<T> {
 }
 
 //------------------- Err Class --------------------
+
+/**
+ * Represents the 'Err' state of a Result object.
+ * @class
+ * @template E - Generic type for 'Err'
+ */
 export class Err<E> {
 	private _value: E;
 
 	constructor(value: E) {
 		this._value = value;
 	}
+
+	/**
+	 * Checks if the Result is in the 'Ok' state.
+	 * @function
+	 * @returns {boolean}
+	 */
 	isOK() {
 		return false;
 	}
-	isErr() {
+
+	/**
+	 * Checks if the Result is in the 'Err' state.
+	 * @function
+	 * @returns {boolean}
+	 */
+	isErr(): boolean {
 		return true;
 	}
-	ok() {
+
+	/**
+	 * Retrieves the 'Ok' value for Result in 'Err' state.
+	 * @function
+	 * @returns {null}
+	 */
+	ok(): null {
 		return null;
 	}
-	err() {
+
+	/**
+	 * Retrieves value in the 'Err' state.
+	 * @function
+	 * @returns {E}
+	 */
+	err(): E {
 		return this._value;
 	}
+
+	/**
+	 * Gets value in the 'Err' state.
+	 * @public
+	 * @readonly
+	 * @returns {E}
+	 */
 	public get errValue(): E {
 		return this._value;
 	}
 
+	/**
+	 * Gets 'Ok' value for Result in 'Err' state.
+	 * @public
+	 * @readonly
+	 * @returns {null}
+	 */
 	public get okValue(): null {
 		return null;
 	}
 
+	/**
+	 * Throws an UnwrapError with a descriptive message when attempting to use unwrap on an 'Err' value.
+	 * @function
+	 * @throws {UnwrapError}
+	 * @returns {void}
+	 */
 	unwrap(): void {
 		throw new UnwrapError(`Called 'Result.unwrap()' on an 'Err' value: ${this._value?.toString()}`);
 	}
+
+	/**
+	 * Retrieves the wrapped error value.
+	 * @function
+	 * @returns {E}
+	 */
 	unwrapErr(): E {
 		return this._value;
 	}
