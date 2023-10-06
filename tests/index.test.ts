23e4774d9de5c813d42d7631493a5488e6b89384
Sylvain Kadjo <sylvainka12@gmail.com>
2023-10-06 17:55:13 +0400

    Test implementation
    - Add test skeleton
    - Add first test for Ok class

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,2 +1,27 @@
-// TODO
-// Add tests here
+import {describe, it, expect} from 'bun:test';
+import {Ok, Err, Result, UnwrapError, matchresult} from '../src/result'
+
+
+describe('Ok', () => {
+  it('Should be an instance of Ok', () => {
+    const ok = new Ok<string>('My ok class');
+    expect(ok).toBeInstanceOf(Ok)
+  });
+
+});
+
+describe('Err', () => {
+  // Implement test for Err class
+});
+
+describe('Result', () => {
+  // Implement test for Result
+});
+
+describe('UnwrapError', () => {
+  // Implement test for UnwrapError
+});
+
+describe('matchresult', () => {
+  // Implement Test for matchresult
+})
