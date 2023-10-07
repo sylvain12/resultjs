2ddc50dabe5a1180018d7d2637c51a5abaab65f2
Sylvain Kadjo <sylvainka12@gmail.com>
2023-10-07 21:15:31 +0400

    Add test for Ok class

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,10 +3,33 @@ import {Ok, Err, Result, UnwrapError, matchresult} from '../src/result'
 
 
 describe('Ok', () => {
+  const value = 'My ok class';
+  const ok = new Ok<string>(value);
+
   it('Should be an instance of Ok', () => {
-    const ok = new Ok<string>('My ok class');
     expect(ok).toBeInstanceOf(Ok)
   });
+
+  it('Should return true when check if object is Ok', () => {
+		expect(ok.isOK()).toBeTrue();
+	});
+
+  it('Should return false when check if object is Err', () => {
+    expect(ok.isErr()).toBeFalse();
+  });
+
+  it('Should return the Ok value', () => {
+    expect(ok.okValue).toBe(value);
+  });
+
+  it('Should return null when trying to get errValue from Ok instance', () => {
+    expect(ok.errValue).toBeNull();
+  });
+
+  it('Should throw an error when trying to unwrap Err value', () => {
+    const fail = ok.unwrapErr;
+    expect(fail).toThrow()
+  })
 
 });
 
