38ce628a02d6d98e3581fc6703a4803058c7f89d
Sylvain Kadjo <sylvainka12@gmail.com>
2023-10-09 00:18:35 +0400

    Add test for Result

--- tests/result.test.ts
+++ tests/result.test.ts
@@ -1,16 +1,90 @@
 import { describe, it, expect } from 'bun:test';
-import { Result, matchresult, UnwrapError } from '../src/result';
+import { Err, Ok, Result, matchresult, UnwrapError } from '../src/result';
+
+
+	interface User{
+		name: string,
+		email: string,
+	}
+
+	const users: User[] = [
+	{
+		name: 'John Doe',
+		email: 'jdoe@email.com',
+	},
+	{
+		name: 'Jane Mary',
+		email: 'jmary@email.com',
+	},
+	{
+		name: 'Dan Smith',
+		email: 'dsmith@email.com',
+	},
+];
+
+	function getUserByEmail(email: string): Result<User, string> {
+		const user = users.find(item => item.email === email);
+		if(user === undefined) {
+			return new Err('User not found')
+		}
+		return new Ok(user);
+	}
 
 describe('Result', () => {
-	// Implement test for Result
+	const existUser = getUserByEmail('jmary@email.com');
+	const nonExistUser = getUserByEmail('smike@email.com');
+
+	// Test for existing user
+	it('Should be instance of Ok', () => {
+		expect(existUser).toBeInstanceOf(Ok);
+	});
+
+	it('Should be true', () => {
+		expect(existUser.isOK()).toBeTrue();
+	});
+
+	it('Should get user value', () => {
+		expect(existUser.okValue?.name).toBe('Jane Mary');
+	});
+
+	// Test for non existing user
+	it('Should be instance of Err', () => {
+		expect(nonExistUser).toBeInstanceOf(Err);
+	});
+
+	it('Should be true', () => {
+		expect(nonExistUser.isErr()).toBeTrue();
+	});
+
+	it('Should get error value', () => {
+		expect(nonExistUser.errValue).toBe('User not found');
+	});
 });
 
 
 describe('matchresult', () => {
-	// Implement Test for matchresult
-});
+	
+	const email = 'dsmith@email.com';
+	const user = getUserByEmail(email);
+	const notFoundUser = getUserByEmail('notfound@email.com');
 
+	const res = matchresult(user)
+	const notFoundRes = matchresult(notFoundUser);
 
-describe('UnwrapError', () => {
-	// Implement test for UnwrapError
+	it('Should get user name', () => {
+		expect(res.ok?.name).toBe('Dan Smith');
+	})
+
+	it('Should be null for err value', () => {
+		expect(res.err).toBeNull()
+	});
+
+	it('Should be null for ok value', () => {
+		expect(notFoundRes.ok).toBe(null);
+	});
+
+	it('Should be: `User not found', () => {
+		expect(notFoundRes.err).toBe('User not found');
+	})
+
 });
