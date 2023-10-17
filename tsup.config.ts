52f50530f7fbb44765b9bdb7d885f4a68da5c803
Sylvain Kadjo <sylvainka12@gmail.com>
2023-10-17 11:32:03 +0400

    fix: build and Publish package
    Add files to include in package and configure tsup for build package

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
 	entry: ['src/result.ts'],
-	splitting: false,
-	sourcemap: true,
 	clean: true,
   format: ['cjs', 'esm'],
   dts: true
